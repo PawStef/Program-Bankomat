bcffbf18c6b6999993176aaca1f525c76dc33574
PawStef <32204923+PawStef@users.noreply.github.com>
2018-08-21 21:15:53 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,6 +13,17 @@ int main()
     if(PIN == 1234)
     {
         cout << "Poprawny PIN" << endl;
+        cout << "Wybierz rodzaj dzialania: " << endl;
+        cout << "1. Wyplac gotowke" << endl;
+        cout << "2. Sprawdz stan konta" << endl;
+        cout << "3. Zmien PIN" << endl;
+        cout << "Wybor: " << endl;
+        int opcja;
+        cin >> opcja;
+
+        if (opcja == 1) {
+            cout << "Podaj kwote " << endl;
+        }
     }
     else
     {
