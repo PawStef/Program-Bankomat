60d98c0f8016de9c296ceba78b0ccfa619c76c5f
PawStef <32204923+PawStef@users.noreply.github.com>
2018-08-22 22:05:07 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,30 +5,34 @@ using namespace std;
 int main()
 {
     cout << "Witaj w naszym banku!" << endl;
-    cout << "Podaj numer PIN: ";
 
-    int PIN;
-    cin >> PIN;
 
-    if(PIN == 1234)
-    {
-        cout << "Poprawny PIN" << endl;
-        cout << "Wybierz rodzaj dzialania: " << endl;
-        cout << "1. Wyplac gotowke" << endl;
-        cout << "2. Sprawdz stan konta" << endl;
-        cout << "3. Zmien PIN" << endl;
-        cout << "Wybor: " << endl;
-        int opcja;
-        cin >> opcja;
+    int pin = 1234;
+    int kod;
+
+    do {
+        cout << "Podaj numer PIN: ";
+        cin >> kod;
+
+        if(pin != kod) {
+           cout << "Kod PIN niepoprawny" << endl;
+        }
+    } while (pin != kod);
+
+    cout << "Poprawny PIN" << endl;
+
+    cout << "Wybierz rodzaj dzialania: " << endl;
+    cout << "1. Wyplac gotowke" << endl;
+    cout << "2. Sprawdz stan konta" << endl;
+    cout << "3. Zmien PIN" << endl;
+    cout << "Wybor: " << endl;
+
+    int opcja;
+    cin >> opcja;
 
         if (opcja == 1) {
             cout << "Podaj kwote " << endl;
         }
-    }
-    else
-    {
-        cout << "Niepoprawny PIN" << endl;
-    }
 
     return 0;
 }
