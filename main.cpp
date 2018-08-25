35fa3f645c1a4ec658ac64ea631edd35efbf9786
PawStef <32204923+PawStef@users.noreply.github.com>
2018-08-25 08:40:10 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,30 +2,13 @@
 
 using namespace std;
 
+int sprawdzeniePin();
+void wyswietlMenu();
+
 int main()
 {
-    cout << "Witaj w naszym banku!" << endl;
-
-
-    int pin = 1234;
-    int kod;
-
-    do {
-        cout << "Podaj numer PIN: ";
-        cin >> kod;
-
-        if(pin != kod) {
-           cout << "Kod PIN niepoprawny" << endl;
-        }
-    } while (pin != kod);
-
-    cout << "Poprawny PIN" << endl;
-
-    cout << "Wybierz rodzaj dzialania: " << endl;
-    cout << "1. Wyplac gotowke" << endl;
-    cout << "2. Sprawdz stan konta" << endl;
-    cout << "3. Zmien PIN" << endl;
-    cout << "Wybor: " << endl;
+    sprawdzeniePin();
+    wyswietlMenu();
 
     int opcja;
     cin >> opcja;
@@ -36,3 +19,31 @@ int main()
 
     return 0;
 }
+
+int sprawdzeniePin() {
+    cout << "Witaj w naszym banku!" << endl;
+    cout << "======================" << endl;
+
+    int pin = 1234;
+    int kod;
+
+    do {
+        cout << "Podaj numer PIN: ";
+        cin >> kod;
+
+        if(pin != kod) {
+           cout << "Kod PIN niepoprawny" << endl;
+           cout << endl;
+        }
+    } while (pin != kod);
+
+    cout << "Poprawny PIN" << endl;
+};
+
+void wyswietlMenu() {
+    cout << "Wybierz rodzaj dzialania: " << endl;
+    cout << "1. Wyplac gotowke" << endl;
+    cout << "2. Sprawdz stan konta" << endl;
+    cout << "3. Zmien PIN" << endl;
+    cout << "Wybor: " << endl;
+};
